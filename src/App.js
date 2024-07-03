18095307a8731dfac958e761f5ebb629dcc3c666
Daniele <info@danielerapisarda.com>
2024-07-03 11:11:07 -0400

    change formattation and layout

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-import Remaining from './components/Remaining';
+import RemainingBudget from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
-
-// Add code to import the other components here under
-
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -19,35 +16,27 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
-                    {
-                        <div className='col-sm'>
-                            <Budget />
-                        </div>
-                    }
-
-                    {
-                        <div className='col-sm'>
-                            <Remaining />
-                        </div>
-                    }
-                    {
-                        <div className='col-sm'>
-                            <ExpenseTotal />
-                        </div>
-                    }
-
-                    {
-                        <div>
-                            <ExpenseList />
-                        </div>
-                    }
-
-                    {
-                        <div>
-                            <AllocationForm />
-                        </div>
-                    }
-
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
