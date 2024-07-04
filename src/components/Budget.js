3f078c5e4f4a126d5a75694f209b6ec9b6cf8732
Daniele Rapisarda <info@danielerapisarda.com>
2024-07-04 12:31:49 +0200

    Budget decoration

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,29 +3,45 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget, dispatch } = useContext(AppContext);
+    let alertType = 'alert-secondary'
+
+    const updateDecoration = () => {
+        alertType = (Number(budget) === 0 )? 'alert-danger' : 'alert-secondary';
+    }
+
+    updateDecoration()
+
     const handleBudgetChange = (event) => {
+        if (event.target.value < 0 ) return
+        
         dispatch({
             type: 'SET_BUDGET',
             payload: event.target.value
         })
+        
+        updateDecoration()
     }
 
     const decreaseBudget = () => {
+        if (budget <= 0 ) return
+        
         dispatch({
             type: 'SET_BUDGET',
             payload: budget - 10
         })
+
+        updateDecoration()
     }
 
     const increaseBudget = () => {
         dispatch({
             type: 'SET_BUDGET',
-            payload: budget + 10
+            payload: Number(budget) + 10
         })
     }
 
     return (
-<div className='alert alert-secondary'>
+<div className={`alert ${alertType}`}>
 <span>Budget: £</span>
 <button onClick={event => increaseBudget()}>+</button>
 <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
